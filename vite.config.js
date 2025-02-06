76af4cc6638f812d56093289dcca0375a3395cdb
gracian-ruiz <gracianmiguel1995@gmail.com>
2025-02-06 19:55:43 +0100

    confio

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,9 @@ export default defineConfig({
         }),
         react(),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        emptyOutDir: true,
+    },
 });
