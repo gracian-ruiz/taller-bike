aa86dabd9bcac55f445e5da49c9238c80b0d693f
gracian-ruiz <gracianmiguel1995@gmail.com>
2025-02-06 19:18:45 +0100

    error middel 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,19 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+    
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+    
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
+        <!-- Inertia.js -->
+        @routes
+        @inertiaHead
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -29,7 +33,7 @@
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                @inertia
             </main>
         </div>
     </body>
